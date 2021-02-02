77c375e42696612b18b4862dd91cd08057a6df74
xiaobin <xiaobin@ruiec.cn>
2021-02-02 16:26:00 +0800

    分包

---
+++ app/src/main/java/com/function/Te.java
@@ -0,0 +1,13 @@
+package com.function;
+
+/**
+ * @author Zero degree
+ * @date 2021/2/2 16:19
+ * @功能:
+ */
+
+class Te {
+    public static void main(String[]args){
+
+    }
+}
---
+++ app/src/main/java/com/function/TestClass.kt
@@ -0,0 +1,11 @@
+package com.function
+
+/**
+ * @author Zero degree
+ * @date 2021/2/2 16:16
+ * @功能:
+ */
+
+class TestClass {
+
+}
